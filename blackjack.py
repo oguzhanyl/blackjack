ab2fd490fa94f0035034ea1f2fcaa5629fd6c49a
Oğuzhan Yılmaz <oguzhan06_61@hotmail.com>
2023-08-29 15:36:37 +0300

    Comparison function, while loop to play again and a few taps. Dinner is ready./Son rütuşlar veeee son.

--- blackjack.py
+++ blackjack.py
@@ -1,4 +1,10 @@
 import random
+import os
+from artblack import logo
+
+# Yeniden oyuna başladığımızda ekranı temizlemek için küçük bir fonksiyon:
+def clear_terminal():
+   os.system("cls" if os.name == "nt" else "clear")
 
 def deal_card():
     """Returns a random card from the cards /Kartlar listesinden rastgele bir kart döndürür."""
@@ -21,37 +27,67 @@ def calculate_score(cards):
     # Koşulların içerisine girerse değişen haliyle girmezse orijinal haliyle kartları toplayıp döndürüyoruz.
     return sum(cards)
 
-user_cards = []
-computer_cards = []
-
-# Oyun sonunu sağlayacağımız kontrol noktası:
-is_game_over = False
-
-# Kullanıcı ve bilgisayarın eline rastgele iki kart veren döngü:
-for _ in range(2):
-    user_cards.append(deal_card())
-    computer_cards.append(deal_card())
-
-# Kart çekme işlemini isteğe bağlı devam ettirecek while döngüsü:
-while not is_game_over:
-    user_score = calculate_score(user_cards)
-    computer_score = calculate_score(computer_cards)
-
-    print(f"    Your cards : {user_cards}, current score : {user_score}")
-    print(f"    Computer's first card : {computer_cards[0]}")
-
-    # Oyun sonu şartlarını taşıyan koşul:
-    if user_score == 0 or computer_score == 0 or user_score > 21:
-        is_game_over = True
+# Kullanıcı ve bilgisayarın skorlarını karşılaştırıp sonucu söyleyen fonksiyon:
+def compare(user_score, computer_score):
+    if user_score == computer_score:
+        return "Draw."
+    elif computer_score == 0:
+        return "Lose, opponent has Blackjack."
+    elif user_score == 0:
+        return "Win with a Blackjack."
+    elif user_score > 21:
+        return "You went over. You lose."
+    elif computer_score > 21:
+        return "Opponent went over. You win."
+    elif user_score > computer_score:
+        return "You win."
     else:
-        # Oyun sonu koşulu sağlanmazsa kart çekme sorusu:
-        user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ")
-        if user_should_deal == 'y':
-            user_cards.append(deal_card())
-        else:
-            is_game_over = True
+        return "You lose."
 
-# Bilgisayarın kartları 0 ile 17 arasındaysa onun destesine otomatik kart ekleyecek while döngüsü:
-while computer_score != 0 and computer_score < 17:
-    computer_cards.append(deal_card())
-    computer_score = calculate_score(computer_cards)
+# Oyunu tekrar oynamak için fonksiyon içerisine alıyoruz.
+def play_game():
+    print(logo)
+    user_cards = []
+    computer_cards = []
+
+    # Oyun sonunu sağlayacağımız kontrol noktası:
+    is_game_over = False
+
+    # Kullanıcı ve bilgisayarın eline rastgele iki kart veren döngü:
+    for _ in range(2):
+        user_cards.append(deal_card())
+        computer_cards.append(deal_card())
+
+    # Kart çekme işlemini isteğe bağlı devam ettirecek while döngüsü:
+    while not is_game_over:
+        user_score = calculate_score(user_cards)
+        computer_score = calculate_score(computer_cards)
+
+        print(f"    Your cards : {user_cards}, current score : {user_score}")
+        print(f"    Computer's first card : {computer_cards[0]}")
+
+        # Oyun sonu şartlarını taşıyan koşul:
+        if user_score == 0 or computer_score == 0 or user_score > 21:
+            is_game_over = True
+        else:
+            # Oyun sonu koşulu sağlanmazsa kart çekme sorusu:
+            user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ")
+            if user_should_deal == 'y':
+                user_cards.append(deal_card())
+            else:
+                is_game_over = True
+
+    # Bilgisayarın kartları 0 ile 17 arasındaysa onun destesine otomatik kart ekleyecek while döngüsü:
+    while computer_score != 0 and computer_score < 17:
+        computer_cards.append(deal_card())
+        computer_score = calculate_score(computer_cards)
+    print(f"    Your final hand: {user_cards}, final score: {user_score}")
+    print(f"    Computer's final hand: {computer_cards}, final score: {computer_score}")
+
+    # Oyun sonu kimin kazandığını öğrenebilmek için compare() fonksiyonunu burada çağırıyoruz:
+    print(compare(user_score, computer_score))
+
+# Oyunu tekrar tekrar oynamak için while döngüsü içerisine fonksiyonu alıyoruz.
+while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == 'y':
+    clear_terminal()
+    play_game()
